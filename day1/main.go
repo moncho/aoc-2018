549f5c5235faf132d0c554fe14f92578f05729f9
moncho <50428+moncho@users.noreply.github.com>
2018-12-02 11:59:34 +0100

    Add day1 part2 solution

--- day1/main.go
+++ day1/main.go
@@ -16,17 +16,49 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	var changes []int
 	sum := 0
+	firstRepeated := 0
+	found := false
+	freqSeen := make(map[int]bool)
+	freqSeen[0] = true
+
 	for scanner.Scan() {
 		i, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			panic(err)
 		}
+		changes = append(changes, i)
 		sum += i
+		if !freqSeen[sum] {
+			freqSeen[sum] = true
+		} else if !found {
+			firstRepeated = sum
+			found = true
+		}
 	}
-
+	firstSol := sum
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
-	fmt.Printf("%d\n", sum)
+
+	for !found {
+		for _, c := range changes {
+			sum += c
+			if !freqSeen[sum] {
+				freqSeen[sum] = true
+			} else if !found {
+				firstRepeated = sum
+				found = true
+				break
+			}
+		}
+	}
+
+	fmt.Printf("Part1 sol: %d\n", firstSol)
+	if found {
+		fmt.Printf("Part2 sol: %d\n", firstRepeated)
+	} else {
+		fmt.Print("Part2 sol: not repeated\n")
+	}
 }
