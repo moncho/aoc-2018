908a393f4eccfae0d3776a2c2044c5f52dc24039
moncho <50428+moncho@users.noreply.github.com>
2018-12-13 22:20:09 +0100

    Add more tests

--- day13/main_test.go
+++ day13/main_test.go
@@ -7,6 +7,11 @@ import (
 	"testing"
 )
 
+const circularGrid = `/----\
+|    |
+|    |
+\----/`
+
 const testGrid = `/->-\        
 |   |  /----\
 | /-+--+-\  |
@@ -29,6 +34,22 @@ v
 ^
 |
 `
+
+const multiCrashGrid = `/>-<\  
+|   |  
+| /<+-\
+| | | v
+\>+</ |
+  |   ^
+  \<->/`
+
+const intersectingGrid = `/-----\
+|     |
+|  /--+--\
+|  |  |  |
+\--+--/  |
+   |     |
+   \-----/`
 
 func Test_cart_forward(t *testing.T) {
 	type fields struct {
@@ -306,10 +327,10 @@ func Test_newGrid(t *testing.T) {
 }
 
 func Test_tick(t *testing.T) {
-	grid, _ := newGrid(strings.NewReader(testGrid))
 	type args struct {
-		c    *cart
-		grid [][]rune
+		ticks int
+		c     *cart
+		grid  string
 	}
 	tests := []struct {
 		name string
@@ -319,12 +340,13 @@ func Test_tick(t *testing.T) {
 		{
 			"cart facing right on horizontal line - moves straight",
 			args{
+				ticks: 1,
 				c: &cart{
 					x:         2,
 					y:         0,
 					direction: faceRight,
 				},
-				grid: grid,
+				grid: testGrid,
 			},
 			&cart{
 				x:         3,
@@ -335,12 +357,13 @@ func Test_tick(t *testing.T) {
 		{
 			"cart facing right on horizontal line - moves straight",
 			args{
+				ticks: 1,
 				c: &cart{
 					x:         3,
 					y:         0,
 					direction: faceRight,
 				},
-				grid: grid,
+				grid: testGrid,
 			},
 			&cart{
 				x:         4,
@@ -351,12 +374,13 @@ func Test_tick(t *testing.T) {
 		{
 			"cart facing right on right turn line - moves down",
 			args{
+				ticks: 1,
 				c: &cart{
 					x:         4,
 					y:         0,
 					direction: faceRight,
 				},
-				grid: grid,
+				grid: testGrid,
 			},
 			&cart{
 				x:         4,
@@ -367,12 +391,13 @@ func Test_tick(t *testing.T) {
 		{
 			"cart facing left on right turn line - moves up",
 			args{
+				ticks: 1,
 				c: &cart{
 					x:         0,
 					y:         4,
 					direction: faceLeft,
 				},
-				grid: grid,
+				grid: testGrid,
 			},
 			&cart{
 				x:         0,
@@ -383,13 +408,14 @@ func Test_tick(t *testing.T) {
 		{
 			"cart facing down at first crossing - turns left",
 			args{
+				ticks: 1,
 				c: &cart{
 					x:         2,
 					y:         4,
 					nextCross: 0,
 					direction: faceDown,
 				},
-				grid: grid,
+				grid: testGrid,
 			},
 			&cart{
 				x:         3,
@@ -401,13 +427,14 @@ func Test_tick(t *testing.T) {
 		{
 			"cart facing down at second crossing - goes straight",
 			args{
+				ticks: 1,
 				c: &cart{
 					x:         2,
 					y:         4,
 					nextCross: 1,
 					direction: faceDown,
 				},
-				grid: grid,
+				grid: testGrid,
 			},
 			&cart{
 				x:         2,
@@ -419,13 +446,14 @@ func Test_tick(t *testing.T) {
 		{
 			"cart facing down at third crossing - turns right",
 			args{
+				ticks: 1,
 				c: &cart{
 					x:         2,
 					y:         4,
 					nextCross: 2,
 					direction: faceDown,
 				},
-				grid: grid,
+				grid: testGrid,
 			},
 			&cart{
 				x:         1,
@@ -437,13 +465,14 @@ func Test_tick(t *testing.T) {
 		{
 			"cart facing up at first crossing - turns left",
 			args{
+				ticks: 1,
 				c: &cart{
 					x:         2,
 					y:         4,
 					nextCross: 0,
 					direction: faceUp,
 				},
-				grid: grid,
+				grid: testGrid,
 			},
 			&cart{
 				x:         1,
@@ -455,13 +484,14 @@ func Test_tick(t *testing.T) {
 		{
 			"cart facing up at second crossing - goes straight",
 			args{
+				ticks: 1,
 				c: &cart{
 					x:         2,
 					y:         4,
 					nextCross: 1,
 					direction: faceUp,
 				},
-				grid: grid,
+				grid: testGrid,
 			},
 			&cart{
 				x:         2,
@@ -473,13 +503,14 @@ func Test_tick(t *testing.T) {
 		{
 			"cart facing up at third crossing - turns right",
 			args{
+				ticks: 1,
 				c: &cart{
 					x:         2,
 					y:         4,
 					nextCross: 2,
 					direction: faceUp,
 				},
-				grid: grid,
+				grid: testGrid,
 			},
 			&cart{
 				x:         3,
@@ -488,10 +519,89 @@ func Test_tick(t *testing.T) {
 				direction: faceRight,
 			},
 		},
+		{
+			"cart moves clockwise on a circular grid",
+			args{
+				ticks: 16,
+				c: &cart{
+					x:         1,
+					y:         0,
+					nextCross: 0,
+					direction: faceRight,
+				},
+				grid: circularGrid,
+			},
+			&cart{
+				x:         1,
+				y:         0,
+				nextCross: 0,
+				direction: faceRight,
+			},
+		},
+		{
+			"cart moves counterclockwise on a circular grid",
+			args{
+				ticks: 16,
+				c: &cart{
+					x:         1,
+					y:         0,
+					nextCross: 0,
+					direction: faceLeft,
+				},
+				grid: circularGrid,
+			},
+			&cart{
+				x:         1,
+				y:         0,
+				nextCross: 0,
+				direction: faceLeft,
+			},
+		},
+		{
+			"cart in a grid with two intersects",
+			args{
+				ticks: 60,
+				c: &cart{
+					x:         1,
+					y:         0,
+					nextCross: 0,
+					direction: faceRight,
+				},
+				grid: intersectingGrid,
+			},
+			&cart{
+				x:         1,
+				y:         0,
+				nextCross: 0,
+				direction: faceRight,
+			},
+		},
+		{
+			"cart in a grid with two intersects",
+			args{
+				ticks: 60,
+				c: &cart{
+					x:         1,
+					y:         0,
+					nextCross: 0,
+					direction: faceLeft,
+				},
+				grid: intersectingGrid,
+			},
+			&cart{
+				x:         1,
+				y:         0,
+				nextCross: 0,
+				direction: faceLeft,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tick(tt.args.c, tt.args.grid)
+			grid, _ := newGrid(strings.NewReader(tt.args.grid))
+			for i := 0; i < tt.args.ticks; i++ {
+				tick(tt.args.c, grid)
+			}
 			if !reflect.DeepEqual(tt.args.c, tt.want) {
 				t.Errorf("after a tick() got cart = %v, want %v", tt.args.c, tt.want)
 			}
@@ -518,6 +628,12 @@ func Test_runSimulation(t *testing.T) {
 			simpleTestGrid,
 			0,
 			3,
+		},
+		{
+			"multicrash grid simulation",
+			multiCrashGrid,
+			2,
+			0,
 		},
 	}
 	for _, tt := range tests {
