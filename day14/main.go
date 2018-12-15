e7c19efdf2fe1af33ec1a7442a784b41ba9ef1b3
moncho <50428+moncho@users.noreply.github.com>
2018-12-15 13:20:52 +0100

    Solve part2

--- day14/main.go
+++ day14/main.go
@@ -10,6 +10,44 @@ func main() {
 	secondElfRecipe := 7
 	s := scores(firstElfRecipe, secondElfRecipe, attempts)
 	fmt.Printf("scores of the ten recipes after %d recipes: %v\n", attempts, s)
+
+	p2 := part2Scores(firstElfRecipe, secondElfRecipe, attempts)
+	fmt.Printf("recipes on the scoreboard to the left of the score sequence %d: %v\n", attempts, len(p2))
+
+}
+func part2Scores(score1, score2, expected int) []int {
+	scores := make([]int, 2, expected+10)
+	scores[0], scores[1] = 3, 7
+	firstElfIndex := 0
+	secondElfIndex := 1
+
+	expectedI := toSlice(expected)
+	le := len(expectedI)
+	scoresL := len(scores)
+
+	var currentRecipe int
+	foundIndex := -1
+	for {
+
+		currentRecipe = scores[firstElfIndex] + scores[secondElfIndex]
+		if currentRecipe > 9 {
+			scores = append(scores, 1)
+			scores = append(scores, currentRecipe%10)
+		} else {
+			scores = append(scores, currentRecipe)
+		}
+		scoresL = len(scores)
+		firstElfIndex = (firstElfIndex + (scores[firstElfIndex] + 1)) % scoresL
+		secondElfIndex = (secondElfIndex + (scores[secondElfIndex] + 1)) % scoresL
+
+		if scoresL > le {
+			foundIndex = index(scores[scoresL-le-1:], expectedI)
+		}
+		if foundIndex >= 0 {
+			break
+		}
+	}
+	return scores[:foundIndex+scoresL-le-1]
 }
 
 func scores(score1, score2, attempts int) []int {
@@ -34,4 +72,46 @@ func scores(score1, score2, attempts int) []int {
 	}
 
 	return scores[count-10 : count]
+}
+
+func eq(aa []int, bb []int) bool {
+	if len(aa) != len(bb) {
+		return false
+	}
+	for i, a := range aa {
+		if a != bb[i] {
+			return false
+		}
+	}
+	return true
+}
+
+//toSlice returns an slice with the digits of
+//the given number
+func toSlice(n int) []int {
+	var result []int
+	for n != 0 {
+		result = append(result, (n % 10))
+		n = n / 10
+	}
+	//reverse
+	for i := len(result)/2 - 1; i >= 0; i-- {
+		opp := len(result) - 1 - i
+		result[i], result[opp] = result[opp], result[i]
+	}
+	return result
+}
+
+//index returns, if present, the index of the first element of the
+//given subarray in the given array. Returns -1 if the subarray is
+//not present
+func index(ints []int, sub []int) int {
+	l := len(sub)
+	for i := 0; i <= len(ints)-l; i++ {
+		if eq(sub, ints[i:i+l]) {
+			return i
+		}
+	}
+
+	return -1
 }
--- day14/main_test.go
+++ day14/main_test.go
@@ -34,3 +34,103 @@ func Test_scores(t *testing.T) {
 		})
 	}
 }
+
+func Test_index(t *testing.T) {
+	type args struct {
+		scores   []int
+		expected []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"",
+			args{
+				[]int{6, 5, 1, 5, 8, 9},
+				[]int{5, 1, 5, 8, 9},
+			},
+			1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := index(tt.args.scores, tt.args.expected); got != tt.want {
+				t.Errorf("index() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_part2Scores(t *testing.T) {
+	type args struct {
+		score1   int
+		score2   int
+		expected int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			"start with scores 3,7 - sequence 51589",
+			args{
+				3,
+				7,
+				51589,
+			},
+
+			[]int{3, 7, 1, 0, 1, 0, 1, 2, 4},
+		},
+		{
+			"start with scores 3,7 - sequence 92510",
+			args{
+				3,
+				7,
+				92510,
+			},
+			[]int{3, 7, 1, 0, 1, 0, 1, 2, 4, 5, 1, 5, 8, 9, 1, 6, 7, 7},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part2Scores(tt.args.score1, tt.args.score2, tt.args.expected); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("part2Scores() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_part2Scores_resultCount(t *testing.T) {
+	type args struct {
+		score1   int
+		score2   int
+		expected int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"start with scores 3,7 - sequence 909441",
+			args{
+				3,
+				7,
+				909441,
+			},
+			20403320,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := part2Scores(tt.args.score1, tt.args.score2, tt.args.expected)
+
+			if len(got) != tt.want {
+				t.Errorf("part2Scores() count = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
