7852d6b100165f7f66fc7b43ee2f2466eaab7dc7
moncho <50428+moncho@users.noreply.github.com>
2019-01-14 01:42:18 +0100

    Solve day17

---
+++ day17/main.go
@@ -0,0 +1,243 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"sort"
+	"strings"
+)
+
+const (
+	clay      = '#'
+	sand      = '.'
+	spring    = '+'
+	water     = '~'
+	waterFlow = '|'
+	springX   = 500
+	springY   = 0
+)
+
+type xy struct {
+	x, y int
+}
+type scanLine struct {
+	fromX, fromY int
+	toX, toY     int
+}
+
+type groundMap struct {
+	terrain    [][]rune
+	minY, maxY int
+	minX, maxX int
+}
+
+func (g groundMap) depth() int {
+	//The last level is below the clay level
+	//and it is not of interest
+	return len(g.terrain) - 1
+}
+
+func (g groundMap) canWaterFlow(x, y int) bool {
+	if x >= g.width() {
+		return false
+	}
+	if y >= g.depth() {
+		return false
+	}
+	return g.terrain[y][x] == sand || g.terrain[y][x] == waterFlow
+}
+func (g groundMap) width() int {
+	return len(g.terrain[0])
+}
+func (g groundMap) spring() (int, int) {
+	return springX - g.minX + 1, springY
+}
+func (g groundMap) waterReach() (int, int) {
+	waterCount := 0
+	flowCount := 0
+	for y := 0; y < g.depth(); y++ {
+		for x := 0; x < g.width(); x++ {
+			if g.terrain[y][x] == water {
+				waterCount++
+			} else if g.terrain[y][x] == waterFlow {
+				flowCount++
+			}
+		}
+	}
+
+	return waterCount, flowCount
+}
+func main() {
+
+	f, err := os.Open("input.txt")
+	defer f.Close()
+
+	if err != nil {
+		panic(err)
+	}
+	s := bufio.NewScanner(f)
+
+	var clay []xy
+	lowestY, maxY := math.MaxInt32, 0
+	lowestX, maxX := math.MaxInt32, 0
+	for s.Scan() {
+		line := s.Text()
+		var s scanLine
+		if strings.HasPrefix(line, "x=") {
+			fmt.Sscanf(line, "x=%d, y=%d..%d", &s.fromX, &s.fromY, &s.toY)
+			s.toX = s.fromX
+		} else {
+			fmt.Sscanf(line, "y=%d, x=%d..%d", &s.fromY, &s.fromX, &s.toX)
+			s.toY = s.fromY
+		}
+		if s.fromY < lowestY {
+			lowestY = s.fromY
+		}
+		if s.toY > maxY {
+			maxY = s.toY
+		}
+		if s.fromX < lowestX {
+			lowestX = s.fromX
+		}
+		if s.toX > maxX {
+			maxX = s.toX
+		}
+		clay = append(clay, scanToXy(s)...)
+	}
+	if s.Err() != nil {
+		panic(err)
+	}
+	sort.Slice(clay, func(i, j int) bool {
+		if clay[i].y == clay[j].y {
+			return clay[i].x < clay[j].x
+		}
+		return clay[i].y < clay[j].y
+	})
+
+	groundMap := newGroundMap(clay, lowestX, lowestY, maxX, maxY)
+
+	x, y := groundMap.spring()
+	simulateWaterFlow(x, y, &groundMap)
+	wc, fc := groundMap.waterReach()
+	fmt.Printf("How many tiles can the water reach within the range of y values in your scan? %d %d, sum: %d\n", wc, fc, wc+fc)
+}
+func newGroundMap(clayScan []xy, minX, minY, maxX, maxY int) groundMap {
+	ground := groundMap{
+		minY: minY,
+		maxY: maxY,
+		minX: minX,
+		maxX: maxX,
+	}
+	//Map starts at ground level(Y = 0), where the spring is located and goes
+	//down infinitely. But since levels below the last level where there is clay
+	//are not of interest the depth is set to be one level below the last level
+	//with clay.
+	depth := maxY - minY + 3
+	//Water might spread to the right or left of the clay
+	width := maxX - minX + 3
+	lines := make([][]rune, depth)
+
+	for line := 0; line < depth; line++ {
+		lines[line] = make([]rune, width)
+		for i := 0; i < width; i++ {
+			lines[line][i] = sand
+		}
+	}
+
+	lines[springY][springX-minX+1] = spring
+	for _, xy := range clayScan {
+		lines[xy.y-minY+1][xy.x-minX+1] = clay
+	}
+	ground.terrain = lines
+	return ground
+}
+func simulateWaterFlow(springX, springY int, gm *groundMap) {
+	//Sources of water flow, the initial source of water is the
+	//spring. Additional sources might appear as water overflows
+	//clay layers.
+	sources := []xy{xy{springX, springY}}
+	for len(sources) > 0 {
+		sourceCount := len(sources)
+		source := sources[sourceCount-1]
+		sources = sources[:sourceCount-1]
+		x, y := source.x, source.y
+		//If still water is found there is nothing left to do
+		//for this source
+		if gm.terrain[y][x] == water {
+			continue
+		}
+
+		//The water flows down, creating new sources of water
+		//where it overflows
+		y++
+		for y < gm.depth() {
+			terrain := gm.terrain[y][x]
+			//Sand found, a flow is created on this level, explore the next level
+			if terrain == sand {
+				gm.terrain[y][x] = waterFlow
+				y++
+			} else if terrain == waterFlow {
+				//A water flow is already present, simulation is back from the level below
+				break
+			} else {
+				//Clay layer, left and right positions are explored to check
+				//whether the current flow overflows (and creates new sources)
+				//or not and it creates a layer of still water
+				y--
+				left := x
+				overFlows := false
+				for gm.canWaterFlow(left, y) {
+					if gm.terrain[y+1][left] == sand {
+						sources = append(sources, xy{left, y})
+						overFlows = true
+						left--
+						break
+					}
+					left--
+				}
+				right := x + 1
+				for gm.canWaterFlow(right, y) {
+					if gm.terrain[y+1][right] == sand {
+						sources = append(sources, xy{right, y})
+						overFlows = true
+						right++
+						break
+					}
+					right++
+				}
+
+				for x := left + 1; x < right; x++ {
+					if overFlows {
+						gm.terrain[y][x] = waterFlow
+					} else {
+						gm.terrain[y][x] = water
+					}
+				}
+			}
+		}
+	}
+}
+
+func scanToXy(s scanLine) []xy {
+	var result []xy
+
+	for x := s.fromX; x <= s.toX; x++ {
+		for y := s.fromY; y <= s.toY; y++ {
+			result = append(result, xy{x, y})
+		}
+	}
+
+	return result
+}
+
+func print(w io.Writer, ground groundMap) {
+	for y := 0; y < ground.depth(); y++ {
+		for x := 0; x < ground.width(); x++ {
+			fmt.Fprintf(w, "%c", ground.terrain[y][x])
+		}
+		fmt.Fprint(w, "\n")
+	}
+}
---
+++ day17/main_test.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func Test_scanToXy(t *testing.T) {
+	type args struct {
+		s scanLine
+	}
+	tests := []struct {
+		name string
+		args args
+		want []xy
+	}{
+		{
+			"x=495, y=2..7",
+			args{
+				s: scanLine{
+					fromX: 495,
+					toX:   495,
+					fromY: 2,
+					toY:   7,
+				},
+			},
+			[]xy{
+				xy{x: 495, y: 2},
+				xy{x: 495, y: 3},
+				xy{x: 495, y: 4},
+				xy{x: 495, y: 5},
+				xy{x: 495, y: 6},
+				xy{x: 495, y: 7},
+			},
+		},
+		{
+			"y=7, x=495..501",
+			args{
+				s: scanLine{
+					fromX: 495,
+					toX:   501,
+					fromY: 7,
+					toY:   7,
+				},
+			},
+			[]xy{
+				xy{x: 495, y: 7},
+				xy{x: 496, y: 7},
+				xy{x: 497, y: 7},
+				xy{x: 498, y: 7},
+				xy{x: 499, y: 7},
+				xy{x: 500, y: 7},
+				xy{x: 501, y: 7},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := scanToXy(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("scanToXy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+const expected = `......+.......
+......|.....#.
+.#..#||||...#.
+.#..#~~#|.....
+.#..#~~#|.....
+.#~~~~~#|.....
+.#~~~~~#|.....
+.#######|.....
+........|.....
+...|||||||||..
+...|#~~~~~#|..
+...|#~~~~~#|..
+...|#~~~~~#|..
+...|#######|..
+`
+
+func Test_flow(t *testing.T) {
+
+	minX := 495
+	maxX := 506
+	minY := 1
+	maxY := 13
+	input := []scanLine{
+		scanLine{fromX: 495, toX: 495, fromY: 2, toY: 7},
+		scanLine{fromY: 7, toY: 7, fromX: 495, toX: 501},
+		scanLine{fromX: 501, toX: 501, fromY: 3, toY: 7},
+		scanLine{fromX: 498, toX: 498, fromY: 2, toY: 4},
+		scanLine{fromX: 506, toX: 506, fromY: 1, toY: 2},
+		scanLine{fromX: 498, toX: 498, fromY: 10, toY: 13},
+		scanLine{fromX: 504, toX: 504, fromY: 10, toY: 13},
+		scanLine{fromY: 13, toY: 13, fromX: 498, toX: 504},
+	}
+
+	var scan []xy
+	for _, s := range input {
+		scan = append(scan, scanToXy(s)...)
+	}
+	groundMap := newGroundMap(scan, minX, minY, maxX, maxY)
+	x, y := groundMap.spring()
+	simulateWaterFlow(x, y, &groundMap)
+	var b strings.Builder
+	print(&b, groundMap)
+
+	if b.String() != expected {
+		t.Errorf("Unexpected state after water flow stabilizes, got:\n%s\n expected:\n%s", b.String(), expected)
+	}
+
+	wc, fc := groundMap.waterReach()
+	if wc+fc != 57 {
+		t.Errorf("Expeted water reach to be 57, got: %d, %d\n", wc, fc)
+	}
+
+}
